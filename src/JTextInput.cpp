b22c118d43e28b60a56b18c2d7960432355cc1dc
Francesco <pham.francesco@gmail.com>
2020-03-15 13:25:08 +0100

    backspace text input

--- src/JTextInput.cpp
+++ src/JTextInput.cpp
@@ -30,6 +30,9 @@ void JTextInput::Update(float dt)
 {
     string input = mEngine->GetTextInput();
     mString += input;
+
+    if(mEngine->GetButtonClick(CTRL_CIRCLE))
+        mString = mString.substr(0, mString.size()-1);
 }
 
 void JTextInput::Render(float x, float y)
